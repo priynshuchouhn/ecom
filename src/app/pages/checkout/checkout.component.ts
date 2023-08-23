e01099e688149f148ce648420177522bde255be8
Priyanshu Chouhan <priyanshu@murchcode.com>
2023-08-23 19:01:46 +0530

    checkout minor fixes

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -7,12 +8,28 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit, OnDestroy {
 
+  constructor(private router: Router){}
+
+  section: string = 'cart';
+
+  user =  JSON.parse(localStorage.getItem('user')!);
+
   ngOnDestroy(): void {
     document.body.style.backgroundColor = "unset";
   }
   ngOnInit(): void {
     document.body.style.backgroundColor = "#f2f2f2";
   }
+
+  onPlaceOrder(){
+    if(this.user){
+      this.section = 'delivery';
+    }else{
+      this.router.navigate(['auth/login']);
+    }
+  }
+
+
 
 
 }
